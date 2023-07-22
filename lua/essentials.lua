e7757f93e3dceb780556f1fc70b921aa0c92de58
fly_aaron.li@outlook.com <fly_aaron.li@outlook.com>
2023-07-22 09:01:51 +0800

    完成基础配置

--- lua/essentials.lua
+++ lua/essentials.lua
@@ -7,7 +7,8 @@
 -- vim.bo.{option} buffer-local 选项
 -- vim.wo.{option} window-local 选项
 -- options多选项：
--- 一般情况下，options设置为：{noremap=true,silent=true}，其中noremap表示不会重新映射，例如：a->b,b->c，如果启用重新映射，则会a->c，->表示映射。 silent为true,表示不会输出多余的信息。
+-- 一般情况下，options设置为：{noremap=true,silent=true}，其中noremap表示不会重新映射，
+-- 例如：a->b,b->c，如果启用重新映射，则会a->c，->表示映射。 silent为true,表示不会输出多余的信息。
 --
 --映射
 local option=vim.opt
@@ -176,24 +177,29 @@ global.mapleader=" "
 
 local keymap=vim.keymap
 
+-- 1. 基础配置
+-- 
+--
 -- 打开要素配置文件
 keymap.set("n","<leader>ce",":e $HOME/.config/nvim/lua/essentials.lua<CR>")
-
+--
 -- 运行配置文件
 keymap.set("n","<leader>cs",":source<CR>")
-
+--
 -- 退出
 keymap.set("n","<A-j>","<ESC>")
 keymap.set("i","<A-j>","<ESC>")
 
--- 跳转Tab
+
+-- 2. 跳转Tab
 --
 -- 
--- 左右跳转
+-- 2.1 左右跳转
 keymap.set("n","<C-h>",":BufferLineCyclePrev<CR>",opt)
 keymap.set("n","<C-l>",":BufferLineCycleNext<CR>",opt)
 -- 
--- 关闭buffer
+--
+-- 2.2 关闭buffer
 keymap.set("n","ww",":w<bar>:Bdelete!<CR>",opt)
 -- 关闭左标签
 keymap.set("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
@@ -201,6 +207,84 @@ keymap.set("n", "<leader>bl", ":BufferLineCloseRight<CR>", opt)
 keymap.set("n", "<leader>bh", ":BufferLineCloseLeft<CR>", opt) 
 -- 选择关闭的标签
 keymap.set("n", "<leader>bc", ":BufferLinePickClose<CR>", opt)
+
+
+-- 3. 窗口设置
+--
+--
+-- 3.1 创建窗口*
+-- 横向创建窗口
+keymap.set("n","<leader>ws","<C-w>s",opt)
+-- 纵向创建窗口
+keymap.set("n","<leader>wv","<C-w>v",opt)
+--
+--
+- 3.2 关闭与离开窗口
+-- 关闭当前窗口
+keymap.set("n","<leader>wc","<C-w>c",opt)
+-- 离开当前窗口
+keymap.set("n","<leader>vq","<C-w>q",opt)
+-- 关闭当前窗口外的所有窗口
+keymap.set("n","<leader>wo","<C-w>o",opt)
+--
+--
+-- 3.3 窗口游走
+-- 
+-- 3.3.1 四个方向游走*
+keymap.set("n","<leader>wj","<C-w>j",opt)
+keymap.set("n","<leader>wk","<C-w>k",opt)
+keymap.set("n","<leader>wh","<C-w>h",opt)
+keymap.set("n","<leader>wl","<C-w>l",opt)
+--
+-- 3.3.2 窗口中循环移动
+keymap.set("n","<leader>ww","<C-w>w",opt)
+--
+-- 3.3.3 对角移动
+-- 移动到最左上角窗口
+keymap.set("n","<leader>wt","<C-w>t",opt)
+-- 移动到最右下角窗口
+keymap.set("n","<leader>wb","<C-w>b",opt)
+-- 
+-- 3.3.4 前访问窗口移动
+keymap.set("n","<leader>wp","<C-w>p",opt)
+--
+--
+-- 3.4 移动窗口
+--
+-- 3.4.1 移动窗口本身
+-- 向右或向下交换窗口
+keymap.set("n","<leader>wr","<C-w>r",opt)
+-- 向左或向上交换窗口
+keymap.set("n","<leader>wR","<C-w>R",opt)
+-- 交换同列或同行的窗口
+keymap.set("n","<leader>wx","<C-w>x",opt)
+-- 
+-- 3.4.2 移动窗口并改变其布局
+-- 移动当前窗口至屏幕顶端，并占全部宽度
+keymap.set("n","<leader>WK","<C-w>K",opt)
+-- 移动当前窗口至屏幕底端，并占全部宽度
+keymap.set("n","<leader>WJ","<C-w>J",opt)
+-- 移动当前窗口至屏幕左端，并占全部宽度
+keymap.set("n","<leader>WH","<C-w>H",opt)
+-- 移动当前窗口至屏幕底端，并占全部宽度
+keymap.set("n","<leader>WL","<C-w>L",opt)
+-- 移动窗口至现有分页
+keymap.set("n","<leader>WT","<C-w>T",opt)
+--
+-- 3.4.3 调整屏幕尺寸
+-- 平均划分所有窗口尺寸
+keymap.set("n","<leader>wa","<C-w>=",opt)
+-- 将当前窗口的高度增加一行
+keymap.set("n","<leader>w=","<C-w>+",opt)
+-- 将当前窗口的高度减少一行
+keymap.set("n","<leader>w-","<C-w>-",opt)
+-- 将当前窗口的宽度增加一行
+keymap.set("n","<leader>w.","<C-w>>",opt)
+-- 将当前窗口的宽度减少一行
+keymap.set("n","<leader>w,","<C-w><",opt)
+-- 将当前窗口的宽度调至最大
+keymap.set("n","<leader>w\\","<C-w>|",opt)
+
 
 -- telescope插件配置
 --

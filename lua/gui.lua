b9c96ca76d7a99fb556a5d9ae5272458d3482bec
fly_aaron.li@outlook.com <fly_aaron.li@outlook.com>
2023-07-25 12:48:27 +0800

    update

---
+++ lua/gui.lua
@@ -0,0 +1,19 @@
+if vim.g.neovide then
+
+    vim.o.guifont = "JetBrainsMono Nerd Font:h13"
+    vim.g.neovide_transparency = 1 -- 透明度
+    vim.g.neovide_hide_mouse_when_typing = true -- 打字时隐藏鼠标
+    vim.g.neovide_confirm_quit = true -- 退出需要确认 
+    vim.g.neovide_fullscreen = true -- 是否全屏
+    vim.g.neovide_enable_ime=true
+    vim.g.neovide_remember_window_size = true -- 记住以前窗口的大小
+    -- 使用super键位,比如<cmd>
+    -- vim.g.neovide_input_use_logo = true
+    -- 开启Alt和Meta按键
+    -- vim.g.neovide_input_macos_alt_is_meta = true
+    -- 触控板死亡地带
+    -- vim.g.neovide_touch_deadzone = 6.0
+    -- 触控板拖动超时
+    -- vim.g.neovide_touch_drag_timeout = 0.17
+    vim.g.neovide_cursor_vfx_mode = "railgun" -- 开启轨道动画
+end
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,97 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig",
+        "folke/neoconf.nvim",
+        "folke/neodev.nvim",
+        {
+            "j-hui/fidget.nvim",
+            tag = "legacy",
+        },
+        {
+            "nvimdev/lspsaga.nvim",
+            dependencies = {
+                'nvim-tree/nvim-web-devicons',     -- optional
+            },
+        },
+    },
+    config = function()
+        require("neoconf").setup()
+        require("neodev").setup()
+        require("fidget").setup()
+        require("lspsaga").setup()
+        require("mason").setup()
+        -- local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        -- 配置文件
+        local servers = {
+            lua_ls = { -- lua
+                Lua = {
+                    workspace = { checkThirdParty = false},
+                    telemetry = { enable = false },
+                },
+            },
+            pyright = {}, -- python
+            jsonls = {}, -- json
+            marksman = {}, -- markdown
+            volar = {}, -- vue
+            dockerls = {}, -- docker
+            docker_compose_language_service = {}, -- docker
+            bashls = {}, -- bash
+            taplo = {}, -- toml
+            clangd = {}, -- c、c++
+            quick_lint_js = {}, -- javascript
+            html = {}, -- html
+            jdtls = {}, -- java
+            -- prettier = {}, -- angular, css, flow, graphql, html, json, jsx, javascript, less, markdown, scss, typescript, vue, yaml
+            -- sql-formatter = {}, -- sql
+            -- cmakelint = {}, -- cmake
+            cmake = {}, -- cmake
+        }
+        -- 当lsp触发时执行任务
+        local on_attach = function(_, bufnr)
+            -- Enable completion triggered by <c-x><c-o>
+            local nmap = function(keys, func, desc)
+                if desc then
+                    desc = 'LSP: ' .. desc
+                end
+
+                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            end
+
+            nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+            nmap('gd', require "telescope.builtin".lsp_definitions, '[G]oto [D]efinition')
+            nmap('K', "<cmd>Lspsaga hover_doc<CR>", 'Hover Documentation')
+            nmap('gi', require "telescope.builtin".lsp_implementations, '[G]oto [I]mplementation')
+            nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+            nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+            nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+            nmap('<leader>wl', function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, '[W]orkspace [L]ist Folders')
+            nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+            nmap('<leader>rn', "<cmd>Lspsaga rename ++project<cr>", '[R]e[n]ame')
+            nmap('<leader>ca', "<cmd>Lspsaga code_action<CR>", '[C]ode [A]ction')
+            nmap('<leader>da', require "telescope.builtin".diagnostics, '[D]i[A]gnostics')
+            nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+            -- nmap('gr', vim.lsp.buf.references, '[G]oto [R]eferences')
+            nmap("<space>f", function()
+                vim.lsp.buf.format { async = true }
+            end, "[F]ormat code")
+        end
+        -- 自动安装、启用、关闭lsp
+        require("mason-lspconfig").setup({
+            -- 通过上方servers告诉lspconfig需要安装的lsp
+            ensure_installed = vim.tbl_keys(servers),
+            handlers = {
+                function(server_name) -- default handler (optional)
+                    require("lspconfig")[server_name].setup {
+                        settings = servers[server_name],
+                        on_attach = on_attach,
+                        -- capabilities = capabilities,
+                    }
+                end,
+            }
+        })
+    end
+}
